fbdc89aa4c49dd00e4d7aedaa3f5f05caddf0c5f
Nadia <nadia_dj@hotmail.co.uk>
2023-01-11 14:34:06 +0000

    completed testing for accessing car properties

--- car.test.js
+++ car.test.js
@@ -1,8 +1,28 @@
 const {Car} = require('./car');
 
-let car; 
+let nadiasCar; 
 
 beforeEach(() => {
-    car = new Car();
+    nadiasCar = new Car("BMW", 200000, "Hybrid");
 })
 
+describe('accessing car properties', () => {
+   test('can access the manufacturer property', () => {
+    expected = "BMW";
+    actual = nadiasCar.manufacturer;
+    expect(actual).toBe(expected);
+
+    }); 
+
+    test('can access price property', () => {
+        expected = 200000;
+        actual = nadiasCar.price;
+        expect(actual).toBe(expected);
+    })
+
+    test('can access engineType property', () => {
+        expected = "Hybrid";
+        actual = nadiasCar.engineType;
+        expect(actual).toBe(expected);
+    })
+})
